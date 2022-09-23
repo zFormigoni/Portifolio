40b1b01a5bb8ba7d1cab883c2cc987e8dfd0f5a2
Formigoni1 <Vitor.formigoni@gmail.com>
2022-09-22 21:01:21 -0300

    header

--- src/render/Sobre.js
+++ src/render/Sobre.js
@@ -3,7 +3,7 @@ import item from "../models/sobre";
 function Sobre() {
   return (
     <>
-      <div id="sobre" className="block">
+      <div id="sobre" className="block sobre-div">
         <div className="container" id="sobre1">
           <div className="row">{item}</div>
         </div>
